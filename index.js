db6a2b9cddc6f3d82f95b7925f89e7d6d0b4430a
Luis Ramos <luissquall@gmail.com>
2015-05-12 23:42:16 -0500

    Init project

---
+++ index.js
@@ -0,0 +1,32 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({
+	host: 'localhost',
+	port: process.env.PORT || 5000
+});
+
+// Add the route
+server.route({
+	method: 'GET',
+	path: '/games/1',
+	handler: function (request, reply) {
+		var data = JSON.stringify(require('./game.json'))
+					.replace(/{{host}}/g, request.info.host);
+
+		reply(data);
+	}
+});
+server.route({
+	method: 'GET',
+	path: '/{param*}',
+	handler: {
+		directory: {
+			path: 'public'
+		}
+	}
+});
+
+// Start the server
+server.start();
