4284f473673259a7bcf0fa4066a88e05fb5a91f7
Luis Ramos <luissquall@gmail.com>
2015-05-13 00:08:34 -0500

    Change host

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Hapi = require('hapi');
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({
-	host: 'localhost',
+	host: '0.0.0.0',
 	port: ~~process.env.PORT || 5000
 });
 
